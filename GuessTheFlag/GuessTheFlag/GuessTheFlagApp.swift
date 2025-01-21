d89919e452f442f11b7316e614b88caa4846be36
Bruno Amazonas Espinace <41213376+Espinace@users.noreply.github.com>
2025-01-21 18:55:38 -0300

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bruno Amazonas Espinace on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
