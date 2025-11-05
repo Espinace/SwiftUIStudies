cadc20fb59cd11bfe702aa7df4b8fcc9ef4ab7e0
Bruno Amazonas Espinace <41213376+Espinace@users.noreply.github.com>
2025-11-05 18:36:29 -0300

    Add files via upload

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Bruno Amazonas Espinace on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
